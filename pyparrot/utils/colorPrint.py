f6d072aec8e4dab3f3b610fe813c2dda24097d2d
Anfernee <anferneejervis@gmail.com>
2019-11-25 16:19:15 -0500

    patch for compatibility with LeapDroneController
    -- messages are printed to stderr instead to support communication
       via stdin/stdout.

--- pyparrot/utils/colorPrint.py
+++ pyparrot/utils/colorPrint.py
@@ -1,6 +1,11 @@
 """
 Print messages in color
+
+Messages are print to stderr to allow compatibility with the LeapDroneController, which communicates via STDIN/STDOUT
 """
+
+import sys
+
 
 def color_print(print_str, type="NONE"):
     # handle null cases
@@ -9,20 +14,20 @@ def color_print(print_str, type="NONE"):
 
     if (type is "ERROR"):
         # red
-        print('\033[38;5;196m %s \033[0m' % print_str)
+        print('\033[38;5;196m %s \033[0m' % print_str, file=sys.stderr)
 
     elif (type is "WARN"):
         # orange
-        print('\033[38;5;202m %s \033[0m' % print_str)
+        print('\033[38;5;202m %s \033[0m' % print_str, file=sys.stderr)
 
     elif (type is "SUCCESS"):
         # green
-        print('\033[38;5;22m %s \033[0m' % print_str)
+        print('\033[38;5;22m %s \033[0m' % print_str, file=sys.stderr)
 
     elif (type is "INFO"):
         # blue
-        print('\033[38;5;33m %s \033[0m' % print_str)
+        print('\033[38;5;33m %s \033[0m' % print_str, file=sys.stderr)
 
     elif (type is "NONE" or type is "DEFAULT"):
         # black
-        print('\033[0m %s \033[0m' % print_str)
+        print('\033[0m %s \033[0m' % print_str, file=sys.stderr)
